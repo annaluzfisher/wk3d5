3848b8cdcdf2665f90cde637adff250f5fc5c901
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-07-01 15:28:44 -0700

    finished tree node project

--- skeleton 2/lib/00_tree_node.rb
+++ skeleton 2/lib/00_tree_node.rb
@@ -1,5 +1,5 @@
 class PolyTreeNode
-attr_reader :value , :parent ,:children
+attr_reader :value , :parent , :children
 
     def initialize(value)
         @value = value 
@@ -11,15 +11,66 @@ attr_reader :value , :parent ,:children
     # should set the node's parent to the new parent
     # should add the node to the new parent's children
     # should remove the node from its old parent's children  
-    def parent=(newNode)
-   
+    def parent=(newnode)
 
-       if @parent == nil
-        @parent = newNode
-        newNode.children << self
+       if @parent.nil?
+        @parent = newnode
+        newnode.children << self
+       elsif newnode.nil?
+        @parent.children.delete(self)
+        @parent = nil
+       elsif @parent != newnode
+        @parent.children.delete(self)
+        @parent = newnode
+        newnode.children << self
+       else
+        self
        end
-      
+       self
     end
-    
-    
+
+    def add_child(newchild)
+        if  !self.children.include?(newchild)
+            newchild.parent = self
+        end
+        self
+    end
+
+    def remove_child(oldchild)
+        oldchild.parent = nil
+    end
+
+    def dfs(target)
+         return self if self.value == target
+         return nil if self.children.length == 0
+        
+         self.children.each do |child|
+            res = child.dfs(target)
+         return res unless res.nil?
+        end
+        return nil
+    end
+
+    def bfs(target)
+        q = Queue.new
+         
+        q.enq(self)
+        while !q.empty?
+            current_node = q.deq
+            if current_node.value == target
+                return current_node
+            end
+                current_node.children.each {|child| q.enq(child)}
+
+        end
+        nil
+    end
+
+   
 end
+
+# a = PolyTreeNode.new("a")
+# b = PolyTreeNode.new("b")
+# c = PolyTreeNode.new("c")
+
+# p a.add_child(c)
