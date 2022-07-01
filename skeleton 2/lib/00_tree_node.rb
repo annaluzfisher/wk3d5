7a36e5c3db6922d99f317c21428df2e147361c1c
MohamedShneshn <98075987+mohamedshneshn@users.noreply.github.com>
2022-07-01 13:31:58 -0700

    first

--- skeleton 2/lib/00_tree_node.rb
+++ skeleton 2/lib/00_tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
+attr_reader :value , :parent ,:children
 
+    def initialize(value)
+        @value = value 
+        @parent = nil
+        @children = []
+    end
+    #"should set a node's parent to the passed node"
+    #"should add the child node to the passed node's children"
+    # should set the node's parent to the new parent
+    # should add the node to the new parent's children
+    # should remove the node from its old parent's children  
+    def parent=(newNode)
+   
+
+       if @parent == nil
+        @parent = newNode
+        newNode.children << self
+       end
+      
+    end
+    
+    
 end
